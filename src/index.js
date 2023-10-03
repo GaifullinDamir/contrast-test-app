e90eb12aeead8e49b1fce1cdb70424101282d6cd
Damir <gaifullindr@gmail.com>
2023-10-04 00:24:26 +0300

    Работа на StartPage

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './css/fonts.css';
+import './css/style.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/StartPage/StartPage.js
+++ src/pages/StartPage/StartPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './startPage.scss';
 
 const StartPage = () => {
   return (
-    <div>
-      StartPage
+    <div className='start-page'>
+      <div className='container'>
+        <div className='start-page__wrapper'>
+          <h1 className='header'>Contrast Test</h1>
+          <div className='descr'>Введите ФИО и номер группы для продолжения</div>
+          <form className='start-page__form'>
+            <input className='input' type='text' placeholder='ФИО'/>
+            <input className='input' type='text' placeholder='Номер группы'/>
+            <button className='button'>Начать</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
