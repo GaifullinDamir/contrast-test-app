c3ae8aad3c091fcd84dfa71344ae26ec11bd413f
Damir <gaifullindr@gmail.com>
2023-10-04 20:47:10 +0300

    Сверстана страница NotFound.

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+    const navigate = useNavigate();
+
+    const onClick = (e) => {
+        e.preventDefault();
+
+        navigate('/');
+    }
     return (
-        <div>
-            NotFound
+        <div className='not-found'>
+            <div className='not-found__wrapper'>
+                <i class="fa fa-chain-broken" aria-hidden="true"></i>
+                <h1 className='header_not-found'>404</h1>
+                <button className='button' onClick={onClick}>На главную</button>
+            </div>
         </div>
     );
 };
