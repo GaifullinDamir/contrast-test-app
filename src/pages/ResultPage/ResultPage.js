5a1c25d5d805f92ba674f683551a420bbc84c6ce
Damir <gaifullindr@gmail.com>
2023-10-04 19:36:26 +0300

    Завершил верстку ResultsPage. Сделал hover для кнопок. Прописал переход по кнопке в окне результата на главную страницу

--- src/pages/ResultPage/ResultPage.js
+++ src/pages/ResultPage/ResultPage.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ResultPage = () => {
+    const navigate = useNavigate();
+    
+    const onClick = (e) => {
+        e.preventDefault();
+
+        navigate('/');
+    }
     return (
         <div className='result-page'>
             <div className='container'>
@@ -14,37 +22,30 @@ const ResultPage = () => {
                             <h1 className='header_table'>Этап 1</h1>
                             <table className='result-page__table'>
                                 <tr className='result-page__table_row'>
-                                    <td className='result-page__table_data'><span>Этап</span></td>
                                     <td className='result-page__table_data'><span>Тест</span></td>
                                     <td className='result-page__table_data'><span>Результат</span></td>
                                 </tr>
                                 <tr className='result-page__table_row'>
                                     <td className='result-page__table_data'>1</td>
-                                    <td className='result-page__table_data'>1</td>
                                     <td className='result-page__table_data'>2/4</td>
                                 </tr>
                                 <tr className='result-page__table_row'>
-                                    <td className='result-page__table_data'>1</td>
                                     <td className='result-page__table_data'>2</td>
                                     <td className='result-page__table_data'>1/4</td>
                                 </tr>
                                 <tr className='result-page__table_row'>
-                                    <td className='result-page__table_data'>1</td>
                                     <td className='result-page__table_data'>3</td>
                                     <td className='result-page__table_data'>1/4</td>
                                 </tr>
                                 <tr className='result-page__table_row'>
-                                    <td className='result-page__table_data'>1</td>
                                     <td className='result-page__table_data'>4</td>
                                     <td className='result-page__table_data'>2/4</td>
                                 </tr>
                                 <tr className='result-page__table_row'>
-                                    <td className='result-page__table_data'>1</td>
                                     <td className='result-page__table_data'>5</td>
                                     <td className='result-page__table_data'>1/4</td>
                                 </tr>
                                 <tr className='result-page__table_row'>
-                                    <td className='result-page__table_data'>1</td>
                                     <td className='result-page__table_data'>6</td>
                                     <td className='result-page__table_data'>1/4</td>
                                 </tr>
@@ -54,43 +55,37 @@ const ResultPage = () => {
                             <h1 className='header_table'>Этап 2</h1>
                             <table className='result-page__table'>
                                 <tr className='result-page__table_row'>
-                                    <td className='result-page__table_data'><span>Этап</span></td>
                                     <td className='result-page__table_data'><span>Тест</span></td>
                                     <td className='result-page__table_data'><span>Результат</span></td>
                                 </tr>
                                 <tr className='result-page__table_row'>
-                                    <td className='result-page__table_data'>2</td>
                                     <td className='result-page__table_data'>1</td>
                                     <td className='result-page__table_data'>2/4</td>
                                 </tr>
                                 <tr className='result-page__table_row'>
                                     <td className='result-page__table_data'>2</td>
-                                    <td className='result-page__table_data'>2</td>
                                     <td className='result-page__table_data'>1/4</td>
                                 </tr>
                                 <tr className='result-page__table_row'>
-                                    <td className='result-page__table_data'>2</td>
                                     <td className='result-page__table_data'>3</td>
                                     <td className='result-page__table_data'>1/4</td>
                                 </tr>
                                 <tr className='result-page__table_row'>
-                                    <td className='result-page__table_data'>2</td>
                                     <td className='result-page__table_data'>4</td>
                                     <td className='result-page__table_data'>2/4</td>
                                 </tr>
                                 <tr className='result-page__table_row'>
-                                    <td className='result-page__table_data'>2</td>
                                     <td className='result-page__table_data'>5</td>
                                     <td className='result-page__table_data'>1/4</td>
                                 </tr>
                                 <tr className='result-page__table_row'>
-                                    <td className='result-page__table_data'>2</td>
                                     <td className='result-page__table_data'>6</td>
                                     <td className='result-page__table_data'>1/4</td>
                                 </tr>
                             </table>
                         </div>
                     </div>
+                    <button className='button' onClick={onClick}>На главную</button>
                 </div>
             </div>
         </div>
