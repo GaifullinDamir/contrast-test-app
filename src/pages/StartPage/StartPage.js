08869fb6071aba16a5df3f2a5427d466e6508b6e
Damir <gaifullindr@gmail.com>
2023-10-05 13:05:52 +0300

    Реализована работа StartPage. Сохранение данных: ФИО и номер группы.

--- src/pages/StartPage/StartPage.js
+++ src/pages/StartPage/StartPage.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const StartPage = () => {
   const navigate = useNavigate();
 
+  const [fullname, setFullname] = useState();
+  const [groupNum, setGroupNum] = useState();
+
+  useEffect(() => {
+    localStorage.removeItem('fullname');
+    localStorage.removeItem('groupNum');
+  }, []);
+  
   const onSubmit = (e) => {
     e.preventDefault();
+    localStorage.setItem('fullname', fullname);
+    localStorage.setItem('groupNum', groupNum);
     navigate('/test');
   }
+
   return (
     <div className='start-page'>
       <div className='container'>
@@ -15,8 +26,8 @@ const StartPage = () => {
           <h1 className='header'>Contrast Test</h1>
           <div className='descr'>Введите <span>ФИО</span> и <span>номер группы</span> <br/> для начала работы.</div>
           <form className='start-page__form' onSubmit={onSubmit}>
-            <input className='input' required type='text' placeholder='ФИО'/>
-            <input className='input' required type='text' placeholder='Номер группы'/>
+            <input className='input' required type='text' placeholder='ФИО' onChange={(e) => setFullname(e.currentTarget.value)}/>
+            <input className='input' required type='text' placeholder='Номер группы' onChange={(e) => setGroupNum(e.currentTarget.value)}/>
             <button className='button'>Начать</button>
           </form>
         </div>
