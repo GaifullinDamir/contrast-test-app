1fe3e3d7bd4edad3cf867cb2deb709bc0b50bbb7
Damir <gaifullindr@gmail.com>
2023-10-06 19:10:17 +0300

    Ра,jnf yfl ltgkjtv

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import './scss/style.scss';
+// import './scss/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/contrast-test-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
